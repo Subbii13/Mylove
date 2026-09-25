c6cb1699633a093c8c964ba32c22c7cde71a5225
Subbii13 <114545896+Subbii13@users.noreply.github.com>
2026-09-25 16:25:44 +0530

    index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "Soloop_20220825202319.mp4");
+  ifrm.setAttribute("src", "Mylove/css/VID-20260925-WA0016.mp4");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
